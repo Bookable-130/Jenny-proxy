39e616671aa294061e4631ae86603fc7c6454332
Jinyeong Park <jinyeongpark1205@gmail.com>
2020-10-07 10:20:25 -0700

    Clean Code

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-// const axios = require('axios');
 const app = express();
 const port = 3000;
 const { createProxyMiddleware } = require('http-proxy-middleware');
