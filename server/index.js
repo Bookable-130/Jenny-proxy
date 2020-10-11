2ec637225832dbe83b7c37924a298168b7dcb658
Jinyeong Park <jinyeongpark1205@gmail.com>
2020-10-10 22:17:50 -0700

    change port in server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port=   3000;
+const port=   3010;
 const { createProxyMiddleware } = require('http-proxy-middleware')
 
 app.use('/rooms/:roomId', express.static(`${__dirname}/../public`));
